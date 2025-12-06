a62767db7567171ffb95f765dd8db1aac2e043b4
Miguel <8240339@estg.ipp.pt>
2025-12-06 14:16:51 +0000

    Português

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Para ti, meu amor`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
